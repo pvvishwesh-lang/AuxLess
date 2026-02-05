8ffd0081e0d9fda19093a9e8b20f144f169551fa
pvvishwesh-lang <pv.vishwesh@gmail.com>
2026-02-05 15:48:48 -0500

    Update csv_writer.py

--- backend/pipelines/api/csv_writer.py
+++ backend/pipelines/api/csv_writer.py
@@ -2,9 +2,9 @@ import io
 import apache_beam as beam
 import csv
 
-class CSVWriter(beam.DoFn):
-    def dict_to_csv_line(record,columns):
-        output = io.StringIO()
-        writer = csv.writer(output)
-        writer.writerow([record.get(col, "") for col in columns])
-        return output.getvalue().strip()
+
+def dict_to_csv_line(record,columns):
+    output = io.StringIO()
+    writer = csv.writer(output)
+    writer.writerow([record.get(col, "") for col in columns])
+    return output.getvalue().strip()

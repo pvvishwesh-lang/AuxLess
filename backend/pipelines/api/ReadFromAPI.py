67564ad992d9d3b98250220e59962d75a5af6bfb
pvvishwesh-lang <pv.vishwesh@gmail.com>
2026-02-04 23:17:45 -0500

    Create ReadFromAPI.py

---
+++ backend/pipelines/api/ReadFromAPI.py
@@ -0,0 +1,66 @@
+import apache_beam as beam
+from time import sleep
+from backend.pipelines.api.YoutubeClient import get_playlists,get_playlist_tracks,get_video_stats
+from backend.pipelines.api.ItunesClient import get_genre
+
+
+
+class ReadFromAPI(beam.DoFn):
+    def __init__(self,access_token):
+        self.access_token=access_token
+        self.genre_cache = {}
+
+    def _retry_request(self,fn,retries=3,delay=2):
+        last_exception=None
+        for i in range(retries):
+            try:
+                return fn()
+            except Exception as e:
+                last_exception=e
+                sleep(delay*(2**i))
+        raise last_exception
+                
+        
+    def process(self,element):
+        playlists=get_playlists()
+        for playlist in playlists:
+            playlist_name=playlist['snippet']['title']
+            playlist_id=playlist['id']
+            tracks=get_playlist_tracks(playlist_id)
+            track_rows=[]
+            video_ids=[]
+            for track in tracks:
+                video_id=track['snippet']['resourceId']['videoId']
+                track_rows.append(track)
+                video_ids.append(video_id)
+
+            stats_lookup={}
+
+            for i in range(0,len(video_ids),50):
+                batch_ids=video_ids[i:i+50]
+                stats_lookup.update(self._retry_request(lambda:get_video_stats(batch_ids)))
+                                    
+            for track in track_rows:
+                track_title=track['snippet']['title']
+                video_id=track['snippet']['resourceId']['videoId']
+                key = f"{track_title}"
+                if key in self.genre_cache:
+                    genre, artist_name, country, collection_name, collection_id, trackTimeMillis = self.genre_cache[key]
+                else:
+                    genre,artist_name,country,collection_name,collection_id,trackTimeMillis=self._retry_request(lambda:get_genre(track_title))
+                    self.genre_cache[key] = (genre, artist_name, country, collection_name, collection_id, trackTimeMillis)
+                view_count,like_count,comment_count=stats_lookup.get(video_id,(0,0,0))
+                yield {
+                    'playlist_name':playlist_name,
+                    'track_title':track_title,
+                    'artist_name':artist_name,
+                    'video_id':video_id,
+                    'genre':genre,
+                    'country':country,
+                    'collection_name':collection_name,
+                    'collection_id':collection_id,
+                    'trackTimeMillis':trackTimeMillis,
+                    'view_count': view_count,
+                    'like_count': like_count,
+                    'comment_count': comment_count
+                }

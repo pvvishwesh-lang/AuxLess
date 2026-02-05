0ad269259f5d356df9c207444bbbb0baddd320b7
pvvishwesh-lang <pv.vishwesh@gmail.com>
2026-02-04 23:09:41 -0500

    Update youtube_client.py

--- backend/pipelines/api/youtube_client.py
+++ backend/pipelines/api/youtube_client.py
@@ -22,3 +22,44 @@ class YoutubeClient:
             else:
                 break
         return all_playlists
+    
+
+
+    def get_playlist_tracks(self,playlist_id):
+        url='https://www.googleapis.com/youtube/v3/playlistItems'
+        params={
+        'part':'snippet,contentDetails',
+        'playlistId':playlist_id,
+        'maxResults':50
+        }
+        all_playlists = []
+        while True:
+            res = requests.get(url, headers=self.headers, params=params)
+            res.raise_for_status()
+            data = res.json()
+            all_playlists.extend(data.get('items', []))
+            if 'nextPageToken' in data:
+                params['pageToken'] = data['nextPageToken']
+            else:
+                break
+        return all_playlists
+    
+    def get_video_stats(self, video_ids):
+        url = "https://www.googleapis.com/youtube/v3/videos"
+        params = {
+        'part': 'statistics',
+        'id': ','.join(video_ids)
+        }
+        res = requests.get(url, headers=self.headers, params=params, timeout=10)
+        res.raise_for_status()
+        data = res.json()
+        stats_map={}
+        for item in data.get('items',[]):
+            vid=item['id']
+            stats=item.get('statistics',{})
+            stats_map[vid]=(
+                int(stats.get('viewCount',0)),
+                int(stats.get('likeCount',0)),
+                int(stats.get('commentCount',0))
+            )
+        return stats_map

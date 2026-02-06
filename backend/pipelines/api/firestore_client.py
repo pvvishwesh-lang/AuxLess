124ea215d357220ff9187358a7d3bf5f37592ad5
pvvishwesh-lang <pv.vishwesh@gmail.com>
2026-02-05 19:45:39 -0500

    Update firestore_client.py

--- backend/pipelines/api/firestore_client.py
+++ backend/pipelines/api/firestore_client.py
@@ -9,9 +9,10 @@ class FirestoreClient:
         doc_ref=self.db.collection(self.collection_name).document(user_id)
         doc_ref.set({
             'refresh_token':refresh_token,
-            'last_active':firestore.SERVER_TIMESTAMP
+            'last_active':firestore.SERVER_TIMESTAMP,
+            'active':True
         })
 
     def get_all_users(self):
-        users=self.db.collection(self.collection_name).stream()
+        users=self.db.collection(self.collection_name).where('active','--',True).stream()
         return [(user.id,user.to_dict()['refresh_token']) for user in users]

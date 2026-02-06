f88297312f4595f199e4dbb2ce2071fc99818a1e
pvvishwesh-lang <pv.vishwesh@gmail.com>
2026-02-05 19:44:06 -0500

    Update Api_Puller.py

--- backend/pipelines/Api_Puller.py
+++ backend/pipelines/Api_Puller.py
@@ -8,39 +8,34 @@ from backend.pipelines.api.csv_writer import dict_to_csv_line
 import os
 
 
-options = PipelineOptions(["--setup_file=./setup.py"])
-google_cloud_options = options.view_as(GoogleCloudOptions)
-google_cloud_options.project = os.environ['PROJECT_ID']
-google_cloud_options.job_name = "youtube-pipeline"
-google_cloud_options.region='us-central1'
-google_cloud_options.staging_location = "gs://youtube-pipeline-staging-bucket/staging"
-google_cloud_options.temp_location = "gs://youtube-pipeline-staging-bucket/temp"
-options.view_as(StandardOptions).runner = "DataflowRunner"
-google_cloud_options.service_account_email='serviceaccountforgithub@main-shade-485500-a0.iam.gserviceaccount.com'
-
-
-
-def run():
+def run_pipeline_for_user(user_id,refresh_token,gcs_prefix):
     auth = GoogleAuthClient(
         token_uri=os.environ["TOKEN_URI"],
         client_id=os.environ["CLIENT_ID"],
         client_secret=os.environ["CLIENT_SECRET"],
         redirect_uri=os.environ["REDIRECT_URIS"],
-        refresh_token=os.environ["YOUTUBE_REFRESH_TOKEN"]
+        refresh_token=refresh_token
     )
     access_token=auth.get_access_token()
     yt_client=YoutubeClient(access_token)
     columns=['playlist_name', 'track_title', 'artist_name', 'video_id', 'genre','country','collection_name','collection_id','trackTimeMillis', 'view_count','like_count','comment_count']
+
+
+    options = PipelineOptions(["--setup_file=./setup.py"])
+    google_cloud_options = options.view_as(GoogleCloudOptions)
+    google_cloud_options.project = os.environ['PROJECT_ID']
+    google_cloud_options.job_name = f"youtube-pipeline--{user_id}"
+    google_cloud_options.region='us-central1'
+    google_cloud_options.staging_location = "gs://youtube-pipeline-staging-bucket/staging"
+    google_cloud_options.temp_location = "gs://youtube-pipeline-staging-bucket/temp"
+    options.view_as(StandardOptions).runner = "DataflowRunner"
+    google_cloud_options.service_account_email='serviceaccountforgithub@main-shade-485500-a0.iam.gserviceaccount.com'
     
     with beam.Pipeline(options=options) as p:
-        data=(
+        (
             p
             |'Seed'>>beam.Create([None])
             |'Read From API'>>beam.ParDo(ReadFromAPI(access_token))
             |'ToCSV' >> beam.Map(lambda r: dict_to_csv_line(r, columns))
-            |'WriteToGCS'>> WriteToText(file_path_prefix='gs://youtube-pipeline-staging-bucket/Final_Output',file_name_suffix='.csv',shard_name_template='',header=','.join(columns))
+            |'WriteToGCS'>> WriteToText(file_path_prefix=f'gs://youtube-pipeline-staging-bucket/{gcs_prefix}{user_id}',file_name_suffix='.csv',shard_name_template='',header=','.join(columns))
         )
-
-
-if __name__=="__main__":
-    run()

e781f6b9ba7d2efca303355f116f1009102cdffc
Vishwesh P V <pv.vishwesh@gmail.com>
2026-02-12 22:22:58 -0500

    Update google_cloud_function.py

--- backend/functions/google_cloud_function.py
+++ backend/functions/google_cloud_function.py
@@ -1,31 +1,47 @@
-import os
-import requests
 import functions_framework
+import os
+from google.cloud import firestore
+from google.cloud import pubsub_v1
 from google.events.cloud.firestore_v1 import DocumentEventData
-from google.auth.transport.requests import Request
-from google.oauth2 import id_token
 
-CLOUD_RUN_URL = os.environ.get("CLOUD_RUN_URL","https://auxless-610684648990.europe-west1.run.app/run_pipeline")
+PROJECT_ID = 'main-shade-485500-a0'
+PUBSUB_TOPIC = 'CloudFunctionPUBSUB'
+
+db=firestore.Client(database='auxless')
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(PROJECT_ID, PUBSUB_TOPIC)
+
+def claim_session(session_id):
+    doc_ref = db.collection("sessions").document(session_id)
+    @firestore.transactional
+    def transaction_claim(transaction):
+        snapshot = doc_ref.get(transaction=transaction)
+        if not snapshot.exists:
+            return False
+        status = snapshot.get("status")
+        if status != "pending":
+            return False
+        transaction.update(doc_ref, {"status": "running"})
+        return True
+    transaction = db.transaction()
+    return transaction_claim(transaction)
 
 @functions_framework.cloud_event
 def firestore_session_trigger(cloud_event):
+    CLOUD_RUN_URL=os.environ.get("CLOUD_RUN_URL","https://auxless-610684648990.europe-west1.run.app/run_pipeline")
     firestore_payload = DocumentEventData.deserialize(cloud_event.data)    
-    doc_snapshot = firestore_payload.value
-    if not doc_snapshot:
+    new_doc = firestore_payload.value
+    session_id = new_doc.name.split("/")[-1]
+    if not new_doc: 
         return
-    session_id = doc_snapshot.name.split("/")[-1]
-    fields = doc_snapshot.fields
-    status = fields.get("status").string_value if "status" in fields else None
-    users_list = fields.get("users").array_value.values if "users" in fields else []
-    print(f"Session {session_id} | Status: {status} | Users count: {len(users_list)}")
-    if status != "pending" or not users_list:
-        return
-    try:
-        auth_req = Request()
-        token = id_token.fetch_id_token(auth_req, CLOUD_RUN_URL)
-        headers = {"Authorization": f"Bearer {token}"}
-        url = f"{CLOUD_RUN_URL}?session_id={session_id}"
-        resp = requests.post(url, headers=headers, timeout=10)
-        print(f"Trigger sent. Result: {resp.status_code}")
-    except Exception as e:
-        print(f"Trigger failed: {e}")
+    new_fields = new_doc.fields
+    new_status = new_fields.get("status").string_value if "status" in new_fields else None    
+    if new_status == "pending":
+        claimed=claim_session(session_id)
+        if not claimed:
+            print('Session claimed. Skipping')
+            return
+        future = publisher.publish(topic_path, session_id.encode("utf-8"))
+        print(f"Published session {session_id} to Pub/Sub: {future.result()}")
+    else:
+        print(f"Ignoring update. Status is {new_status}")

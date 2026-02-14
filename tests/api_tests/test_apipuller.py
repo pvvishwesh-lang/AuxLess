cbceef4805570590224e46ca9b2b5b293ed0845f
Vishwesh P V <pv.vishwesh@gmail.com>
2026-02-14 17:04:33 -0500

    Update test_apipuller.py

--- tests/api_tests/test_apipuller.py
+++ tests/api_tests/test_apipuller.py
@@ -12,7 +12,7 @@ os.environ["REDIRECT_URIS"]="test"
 
 @pytest.fixture(autouse=True)
 def mock_auth(monkeypatch):
-    monkeypatch.setattr("backend.pipelines.Api_Puller.auth.get_access_token",lambda: "fake_access_token")
+    monkeypatch.setattr("backend.pipelines.Api_Puller.GoogleAuthClient.get_access_token",lambda: "fake_access_token")
 
 def test_pipeline_for_multiple_users(monkeypatch):
   class FakeDoFn:

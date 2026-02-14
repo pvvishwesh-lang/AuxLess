40fb02b1dc2ff29b5c31897e9c9ebdc3190c8286
Vishwesh P V <pv.vishwesh@gmail.com>
2026-02-14 13:45:43 -0500

    Delete tests/api_tests/test_pipeline.py

--- tests/api_tests/test_pipeline.py
+++
@@ -1,9 +0,0 @@
-import os
-import pytest
-
-@pytest.mark.skipif(os.getenv("CI") == "true", reason="Skipped in CI")
-def test_training():
-    return None
-
-if __name__=="__main__":
-  print(test_training())

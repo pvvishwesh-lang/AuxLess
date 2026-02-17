5d5753a0e25aab573b8fbc085a8aaeb692b3bee4
Vishwesh P V <pv.vishwesh@gmail.com>
2026-02-17 18:54:42 -0500

    Start streaming music analytics in background
    Add background process for streaming music analytics.
    
    Signed-off-by: Vishwesh P V <pv.vishwesh@gmail.com>

--- backend/pipelines/run_all_users.py
+++ backend/pipelines/run_all_users.py
@@ -71,3 +71,12 @@ def run_for_session(session_id):
     except Exception as e:
         print(f'Combination failed for {session_id}:{e}')
         fs.update_session_status(session_id, "error")
+    try:
+        subprocess.Popen(
+            ["python", "backend/pipelines/streaming_music_analytics.py"],
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        print("Streaming feedback pipeline started in background.")
+    except Exception as e:
+        print(f"Failed to start streaming pipeline: {e}")

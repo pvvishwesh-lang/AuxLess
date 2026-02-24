b203666abc66ffdd09f0c1b39062491fc19f8c1b
Vishwesh P V <pv.vishwesh@gmail.com>
2026-02-23 22:52:34 -0500

    Replace ReadFromAPI with ValidatingDoFn in pipeline
    Signed-off-by: Vishwesh P V <pv.vishwesh@gmail.com>

--- backend/pipelines/Api_Puller.py
+++ backend/pipelines/Api_Puller.py
@@ -49,7 +49,7 @@ def run_pipeline_for_user(user_id,refresh_token,gcs_prefix,session_id):
     (
             p
             |'Seed'>>beam.Create([None])
-            |'Read From API'>>beam.ParDo(ReadFromAPI(access_token))
+            |'Read From API' >> beam.ParDo(ValidatingDoFn(access_token, session_id=session_id, user_id=user_id))
             |'ToCSV' >> beam.Map(lambda r: dict_to_csv_line(r, columns))
             |'WriteToGCS'>> WriteToText(file_path_prefix=f'gs://youtube-pipeline-staging-bucket/{gcs_prefix}{user_id}',file_name_suffix='.csv',shard_name_template='',header=','.join(columns))
     )

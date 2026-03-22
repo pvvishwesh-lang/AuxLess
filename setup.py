b0b518ddb95c2780187785c7f53361d8cf1ee3d0
Vishwesh P V <pv.vishwesh@gmail.com>
2026-03-22 01:31:18 -0400

    Update setup.py
    Signed-off-by: Vishwesh P V <pv.vishwesh@gmail.com>

--- setup.py
+++ setup.py
@@ -4,4 +4,13 @@ setup(
     name="auxless",
     version="0.1.0",
     packages=find_packages(),
+    install_requires=[
+        "google-cloud-firestore",
+        "google-cloud-storage",
+        "google-cloud-pubsub",
+        "requests",
+        "pandas",
+        "numpy",
+        "scipy",
+    ],
 )
